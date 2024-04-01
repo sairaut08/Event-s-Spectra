da17384fe01027e314b31150e48a7a4fe674c786
Sarvesh Sambhaji Gawade <sarveshgawadesg@gmail.com>
2024-04-01 17:24:20 +0530

    added comment

--- Client_/src/App.jsx
+++ Client_/src/App.jsx
@@ -14,6 +14,8 @@ function App() {
   
 
   return (
+
+    // routes 
     <Routes>
         <Route path='/' element={ <HomePage/> } />
         <Route path='/about-us' element={ <AboutUsPage/> } />
