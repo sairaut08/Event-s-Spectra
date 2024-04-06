b95666aa590f99555f8f407cd49e541c081ae9f9
Sarvesh Sambhaji Gawade <sarveshgawadesg@gmail.com>
2024-04-06 20:09:10 +0530

    implemented persist-storage

--- Client_/src/Redux/store.js
+++ Client_/src/Redux/store.js
@@ -2,12 +2,40 @@ import {configureStore} from '@reduxjs/toolkit'
 import authSliceReducer from './Slices/authSlice'
 import clubSliceReducer from './Slices/clubSlice'
 
+// for persist-storage
+import storage from 'redux-persist/lib/storage'
+import {persistReducer} from 'redux-persist'
+import {combineReducers} from '@reduxjs/toolkit'
+// import persistReducer from 'redux-persist/es/persistReducer'
+
+const persistConfig = {
+    key: 'root' ,
+    version: 1 ,
+    storage
+}
+
+const reducer = combineReducers({
+    auth: authSliceReducer ,
+    clubs: clubSliceReducer
+})
+
+const persistedReducer = persistReducer(persistConfig,reducer)
+
+
 const store = configureStore({
-    reducer: {
-        auth: authSliceReducer ,
-        clubs: clubSliceReducer
-    } ,
+    reducer: persistedReducer,
     devTools: true
 })
+
+
+// NORMAL STORE BELOW
+
+// const store = configureStore({
+//     reducer: {
+//         auth: authSliceReducer ,
+//         clubs: clubSliceReducer
+//     } ,
+//     devTools: true
+// })
 
 export default store
